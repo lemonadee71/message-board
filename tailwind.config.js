04da0dbb418fa599dc69f17a11b0c862526ae9ba
Shin Andrei Riego <riegoshinandrei@gmail.com>
2022-11-01 20:41:54 +0800

    Add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  // eslint-disable-next-line
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
