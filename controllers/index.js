a30203029e3c0cc4eb06fede39a6aa9320025c2e
Shin Andrei Riego <riegoshinandrei@gmail.com>
2022-11-03 19:06:33 +0800

    Add basic routes and controllers for authentication

---
+++ controllers/index.js
@@ -0,0 +1,71 @@
+const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
+const passport = require('passport');
+const User = require('../models/user');
+
+module.exports = {
+  home: (req, res) => {
+    res.render('index', { title: "Lemon's Message Board" });
+  },
+  signup: {
+    get: (req, res) => {
+      res.render('signup');
+    },
+    post: [
+      body('username')
+        .trim()
+        .escape()
+        .isAlphanumeric()
+        .withMessage('Username has non-alphanumeric characters')
+        .isLength({ min: 4, max: 30 })
+        .withMessage(
+          'Username must be at least 4 characters and no more than 30 characters'
+        )
+        .custom((value) =>
+          // Might throw an error unrelated to validation (fetching errors)
+          User.findById(value).then(
+            (res) =>
+              res &&
+              Promise.reject(
+                new Error(`User with the username, ${value} already exists`)
+              )
+          )
+        ),
+      body('password')
+        .trim()
+        .escape()
+        .isStrongPassword()
+        .withMessage('Password must be a strong password'),
+      (req, res, next) => {
+        const errors = validationResult(req);
+        const { username, password } = req.body;
+
+        if (errors.isEmpty()) {
+          new User({ username, password }).save(() => next());
+        } else {
+          res.render('signup', { errors: errors.array() });
+        }
+      },
+      passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/',
+      }),
+    ],
+  },
+  login: {
+    get: (req, res) => {
+      res.render('login');
+    },
+    post: passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/signup',
+    }),
+  },
+  logout: (req, res, next) => {
+    req.logout((err) => {
+      if (err) return next(err);
+
+      res.redirect('/');
+    });
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const { getMessages } = require('./state');
+const controller = require('../controllers');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: "Lemon's Message Board",
-    messages: getMessages(),
-  });
-});
+router.get('/', controller.home);
+router.get('/signup', controller.signup.get);
+router.post('/signup', controller.signup.post);
+router.get('/login', controller.login.get);
+router.post('/login', controller.login.post);
+router.get('/logout', controller.logout);
 
 module.exports = router;
