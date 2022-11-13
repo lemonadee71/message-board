651172582e3804d193af05a7a0aa7d6c86208a9c
Shin Andrei Riego <riegoshinandrei@gmail.com>
2022-11-13 18:27:06 +0800

    refactor(views): Use stringify on other places

--- filters.js
+++ filters.js
@@ -3,6 +3,8 @@ exports.stringify = (o, delimiter = ' ') =>
     .map(([key, value]) => {
       if (value === '' || (typeof value === 'boolean' && value)) return key;
       if (!value) return '';
+      // not having quotes will break for space separated values
+      // but having them results in a duplicate
       return `${key}=${value}`;
     })
     .join(delimiter);
